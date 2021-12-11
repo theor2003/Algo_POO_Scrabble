6067786902b13ce27d417a80fc67558a3ce60c2d
Maëlle LEMAIGNEN <maelle.lemaignen@edu.devinci.fr>
2021-12-11 14:11:37 +0100

    dicoo

--- Projet Scrabble/Projet Scrabble/Dictionnaire.cs
+++ Projet Scrabble/Projet Scrabble/Dictionnaire.cs
@@ -41,6 +41,33 @@ public class Dictionnaire
 				nbmot = nbmot + 1;
 			}
 		}
-		return "Il y a " + nbmot + " dans le dictionnaire francais.";
+		return "Il y a " + nbmot + " dans le dictionnaire francais."; // je sais c'est pas ca mais ca peux aider 
+		
+	}
+
+	public bool RechDichoRecursif(int debut, int fin, string mot)
+	{
+		int tailleMot = mot.Length;
+		int milieu = (debut + fin) / 2;
+		int resultat = String.Compare(mot, dico[tailleMot - 2][milieu]);
+
+		if (debut > fin)
+        {
+			return false;
+        }
+		else if (resultat == 0)
+		{
+			return true;
+		}
+		else if(resultat > 0)
+        {
+			return RechDichoRecursif(milieu + 1, fin, mot);
+        }
+		else if(resultat < 0)
+        {
+			return RechDichoRecursif(debut , milieu - 1,mot);
+        }
+		
+		
 	}
 }
