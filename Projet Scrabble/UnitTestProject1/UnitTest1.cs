bc03aca2a99710f99be68860ffece9349f47e49c
Maëlle LEMAIGNEN <maelle.lemaignen@edu.devinci.fr>
2021-12-12 23:43:34 +0100

    test unit qui marche pas

--- Projet Scrabble/UnitTestProject1/UnitTest1.cs
+++ Projet Scrabble/UnitTestProject1/UnitTest1.cs
@@ -10,24 +10,24 @@ namespace UnitTest1
         public void TestToStringJeton()
         {
             string lettre = "A";
-            int scorelettre = 0;
-            int quantite = 0;
-            string resultat = "A ;";
+            int scorelettre = 1;
+            int quantite = 9;
+            string resultat = "A;";
             Jeton jeton = new Jeton(lettre, scorelettre, quantite);
             string jet = jeton.ToString();
             Assert.AreEqual<string>(resultat, jet);
         }
 
         [TestMethod]
-        public void TestToStringSac_Jeton()
+        public void TestToStringSac_Jetons()
         {
-            Jeton j1 = new Jeton("A", 0, 0);
+            Jeton j1 = new Jeton("A", 1, 9);
             Jeton j2 = new Jeton("B", 0, 0);
-            Sac_Jeton s = new Sac_Jeton();
-            s.ListeJeton.Add(j1);
-            s.ListeJeton.Add(j2);
+            Sac_Jeton sac = new Sac_Jeton();
+            sac.jeton.Add(j1);
+            sac.jeton.Add(j2);
             string resultat = "Ce sac contient : \nA ; B ; ";
-            string c = s.toString();
+            string c = sac.ToString();
             Assert.AreEqual<string>(resultat, c);
 
         }
@@ -35,19 +35,19 @@ namespace UnitTest1
         [TestMethod]
         public void TestToStringDictionnaire()
         {
-            Dictionnaire dico = new Dictionnaire(patch);
+            Dictionnaire dico = new Dictionnaire("A", "A") ;
             string resultat = "Il y a 0 dans le dictionnaire.";
-            string d = dico.toString();
+            string d = dico.ToString();
             Assert.AreEqual<string>(resultat, d);
         }
 
-        [TestMethod]
-        public void TestToStringJoueur()
+       /* [TestMethod]
+        //public void TestToStringJoueur()
         {
             string nom = "A";
-            int score = 0;
-            string resultat =
-            }
+            int score = 1;
+            string resultat = 
+            }*/
 
 
 
