b91907ba993215a5ae4353c13666e70a98576563
Maëlle LEMAIGNEN <maelle.lemaignen@edu.devinci.fr>
2021-12-13 21:38:22 +0100

    TEST1

--- Projet Scrabble/UnitTestProject1/UnitTest1.cs
+++ Projet Scrabble/UnitTestProject1/UnitTest1.cs
@@ -12,42 +12,13 @@ namespace UnitTest1
             string lettre = "A";
             int scorelettre = 1;
             int quantite = 9;
-            string resultat = "A;";
+            string resultat = "Le jeton a pour lettre : A qui a pour score : 1et il y en a : 9";
             Jeton jeton = new Jeton(lettre, scorelettre, quantite);
             string jet = jeton.ToString();
-            Assert.AreEqual<string>(resultat, jet);
+            Assert.AreEqual(resultat, jet);
         }
 
-        [TestMethod]
-        public void TestToStringSac_Jetons()
-        {
-            Jeton j1 = new Jeton("A", 1, 9);
-            Jeton j2 = new Jeton("B", 0, 0);
-            Sac_Jeton sac = new Sac_Jeton();
-            sac.jeton.Add(j1);
-            sac.jeton.Add(j2);
-            string resultat = "Ce sac contient : \nA ; B ; ";
-            string c = sac.ToString();
-            Assert.AreEqual<string>(resultat, c);
 
-        }
-
-        [TestMethod]
-        public void TestToStringDictionnaire()
-        {
-            Dictionnaire dico = new Dictionnaire("A", "A") ;
-            string resultat = "Il y a 0 dans le dictionnaire.";
-            string d = dico.ToString();
-            Assert.AreEqual<string>(resultat, d);
-        }
-
-       /* [TestMethod]
-        //public void TestToStringJoueur()
-        {
-            string nom = "A";
-            int score = 1;
-            string resultat = 
-            }*/
 
 
 
